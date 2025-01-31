b5eb1d4088f3b3863e9c73129cde0e2f0b4af8ad
abdullahyzgc <mavihosttr@gmail.com>
2025-01-31 22:31:29 +0300

    UPDATE MAİN PAGE

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,8 @@ export default function HomePage() {
   return (
     <div className="flex flex-col py-8">
       <h1 className="text-balance mb-8 text-center text-4xl font-extrabold">
-        Instagram Video Downloader
+        HI TEST EVERYONE!
       </h1>
-      <section className="flex flex-col items-center justify-center gap-4">
-        <InstagramVideoForm />
-      </section>
     </div>
   );
 }
